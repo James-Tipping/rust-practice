e7fd07839756f507b7df43eca59343398b7d7fc0
Your Name <jamestipping@protonmail.com>
2025-08-20 22:24:27 +0100

    example program

--- chapter5/rectangles/src/main.rs
+++ chapter5/rectangles/src/main.rs
@@ -1,3 +1,22 @@
 fn main() {
-    println!("Hello, world!");
+    let rectangle1 = Rectangle {
+        width: 10,
+        height: 15,
+    };
+
+    let rectange1_area = area(&rectangle1);
+
+    println!("rectange1_area: {}", rectange1_area);
+
+    println!("rectangle1: {:#?}", rectangle1);
+}
+
+fn area(rectangle: &Rectangle) -> u32 {
+    return rectangle.width * rectangle.height;
+}
+
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32,
 }
